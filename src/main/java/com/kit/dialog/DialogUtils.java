7abd876fc26c3a997a0f7fe7450d7b255da0d091
joeyzhao <joeyzhao1005@gmail.com>
2018-02-02 17:39:13 +0800

    intent 跳转重构

--- src/main/java/com/kit/dialog/DialogUtils.java
+++ src/main/java/com/kit/dialog/DialogUtils.java
@@ -6,51 +6,50 @@ import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.kit.extend.widget.R;
-import com.kit.utils.intentutils.IntentUtils;
+import com.kit.utils.intentutils.IntentManager;
 
 public class DialogUtils {
-	public static DefaultDialog showNetWorkErrorDialog(final Context context,
-			final Class<?> cls, boolean isClose) {
-		final DefaultDialog dd = new DefaultDialog(context, "网络可能有误，请配置网络",
-				R.layout.dialog_default, false);
-		dd.show();
+    public static DefaultDialog showNetWorkErrorDialog(final Context context,
+                                                       final Class<?> cls, boolean isClose) {
+        final DefaultDialog dd = new DefaultDialog(context, "网络可能有误，请配置网络",
+                R.layout.dialog_default, false);
+        dd.show();
 
-		dd.mButtonOK.setOnClickListener(new OnClickListener() {
+        dd.mButtonOK.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
 
-				dd.cancel();
+                dd.cancel();
 
-				IntentUtils.gotoNextActivity(context, cls);
+                IntentManager.get().setClass(context, cls).startActivity(context);
 
-			}
-		});
-		if (isClose) {
-			((Activity) context).finish();
-		}
-		return dd;
+            }
+        });
+        if (isClose) {
+            ((Activity) context).finish();
+        }
+        return dd;
 
-	}
+    }
 
 
-	/**
-	 * 展示只有一个按钮的对话框
-	 * @param context
-	 * @param onClickListener
+    /**
+     * 展示只有一个按钮的对话框
+     *
+     * @param context
+     * @param onClickListener
      * @return
      */
-	public static DefaultDialog showOneButtonDialog(final Context context,String msg,OnClickListener onClickListener) {
-		final DefaultDialog dd = new DefaultDialog(context, msg,
-				R.layout.dialog_ok, false);
-		dd.show();
-		dd.mButtonOK.setOnClickListener(onClickListener);
+    public static DefaultDialog showOneButtonDialog(final Context context, String msg, OnClickListener onClickListener) {
+        final DefaultDialog dd = new DefaultDialog(context, msg,
+                R.layout.dialog_ok, false);
+        dd.show();
+        dd.mButtonOK.setOnClickListener(onClickListener);
 
-		return dd;
+        return dd;
 
-	}
-
-
+    }
 
 
 }
